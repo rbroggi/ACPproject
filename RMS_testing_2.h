fb9da869057e54058c1443b0be043396f550a7a0
Rodrigo Broggi <ro_broggi@hotmail.com>
2013-12-15 10:34:49 +0100

    Fatto Spearsman rho

--- RMS_testing_2.h
+++ RMS_testing_2.h
@@ -12,8 +12,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
-
+#include <algorithm>  //use sort
+#include <cmath>      //use log2 (just c++11)
+//#include "gnuplot-iostream.h" //for draw_ROC
 
 using namespace std;
 
@@ -42,7 +43,15 @@ void error (string & str){
     cerr<<str<<endl;
     exit(1);
     
-}
+};
+
+bool comparison_func_rate (const pair<int,double > & a, const pair<int,double > & b){
+    return (a.second > b.second);
+};
+
+bool comparison_func_item (const pair<int,int > & a, const pair<int,int > & b){
+    return (a.first < b.first);
+};
 
 
 
@@ -292,6 +301,29 @@ double AUC_ROC (const vector<data> & relevant,const vector<data> & not_relevant)
     
 };
 
+//void draw_ROC (const vector<data> & relevant,const vector<data> & not_relevant){
+//    
+//    vector<double> recall_n; recall_n.push_back(0);
+//    
+//    vector<double> fallout_n; fallout_n.push_back(0);
+//    
+//    Gnuplot gp;
+//    
+//    for (int i = 1; recall_n.back() < 1 || fallout_n.back() < 1 ; i++) {
+//        recall_n.push_back(RMS_recall(relevant,i));
+//        fallout_n.push_back(RMS_fallout(not_relevant,i));
+//    }
+//    
+//
+//    //impostando parametri grafico:
+//    gp << "set xrange [0:1]\nset yrange [0:1]\n";
+//    
+//	// '-' means read from stdin.  The send1d() function sends data to gnuplot's stdin.
+//	gp << "plot '-' with vectors title 'pts_A', '-' with vectors title 'pts_B'\n";
+//	gp.send1d(fallout_n);
+//	gp.send1d(recall_n);
+//}
+
 //Funzionante
 double RMS_ARHR (const vector<data> & relevant, const int N){
     
@@ -345,6 +377,190 @@ double RMS_ARP (const vector<data> & relevant, const int N){
     double h = hits;
     
     return (numerator/hits);
+    
+};
+
+//Usa funzione log2() - solo c++11
+double RMS_DCG (const vector<data> & relevant, const int N){
+    
+    if (relevant.size() == 0) {
+        
+        string s1("The relevant data must have contents! ");
+        error(s1);
+        
+    }
+    
+    
+    double numerator = 0;
+    int hits = 0;
+    
+    for (auto it = relevant.begin(); it != relevant.end(); it++) {
+        if (it->my_pos() <= N){
+            double temp= max(1.0,log2(it->my_pos()));
+            numerator+=(1.0/temp);
+            hits++;
+        }
+    }
+    
+    double h = hits;
+    
+    return (numerator/hits);
+    
+};
+
+//In classifica ci sono i rank degli item per un determinato user: Importante notare che ogni indice rappresenta un item, quindi
+//l'indice "[i]" rappresenta lo stesso item per entrambi vettori classifica_1 e classifica_2 vettori
+//Oss.: Creddo che sia più intelligente passare così gli argomenti perché non c'è bisogno che si passi la URM intera.
+//PARALLELIZABILE
+double s_rho (const vector<int> & classifica_1, const vector<int> & classifica_2){
+    
+    if (classifica_1.size() != classifica_2.size()) {
+        string s1("In Spearmens rho computation, both ranks must equal on number! ");
+        error(s1);
+    }
+    
+    double r_average_1 = 0;
+    double r_average_2 = 0;
+    
+    for (int i = 0; i < classifica_1.size(); i++) {
+        r_average_1 += classifica_1[i];
+        r_average_2 += classifica_2[i];
+    }
+    
+    r_average_1 = r_average_1/classifica_1.size();
+    r_average_2 = r_average_2/classifica_1.size();
+    
+    double contributo = 0;
+    
+    double std_1 = 0;
+    double std_2 = 0;
+    
+    for (int i = 0; i < classifica_1.size(); i++) {
+        contributo += ((classifica_1[i] - r_average_1)*(classifica_2[i] - r_average_2));
+        std_1 += ((classifica_1[i] - r_average_1)*(classifica_1[i] - r_average_1));
+        std_2 += ((classifica_2[i] - r_average_2)*(classifica_2[i] - r_average_2));
+    }
+    
+    std_1 = sqrt((1.0/classifica_1.size())*std_1);
+    std_2 = sqrt((1.0/classifica_1.size())*std_2);
+    
+    
+    return ((1.0/classifica_1.size())*contributo*(1.0/(std_1*std_2)));
+};
+
+//Questa funzione gestisce i dati previsti e reali di un'uttente in modo da usare la funzione "s_rho" nel modo correto, Nel senso che mette a posto i vettori nel
+//modo in cui bisogna chiamare la funzione s_rho
+//In INPUT si attente un vettore previsioni e un vettore real con dentro i rispettivi ratting (accetano anche vettori con rate non assegnati (convenzione = -1))
+//complessità nlogn (uso sort)
+double compute_s_rho (const vector<pair<int,double> > & prevision,const vector<pair<int,double> > & real){
+
+    //per poter non cambiare l'ordine dei vettori in entrata
+    vector<pair<int,double> > p = prevision;
+    vector<pair<int,double> > r = real;
+    
+    //ordina in base al rating (in ordine decrescente)
+    sort(p.begin(),p.end(),comparison_func_rate);
+    sort(r.begin(),r.end(),comparison_func_rate);
+    
+    //taglio gli elementi senza rate:
+    auto it = p.begin();
+    auto it2 = r.begin();
+    
+    while(it != p.end() || it->second == -1) {
+        it++;
+    }
+    
+    while(it2 != r.end() || it2->second == -1) {
+        it2++;
+    }
+    
+    if (it != p.end())
+        p.erase(it,p.end());
+
+    
+    if (it2 != r.end())
+        r.erase(it2,r.end());
+
+    
+    //in questi metto per ogni item il suo corrisondente rank
+    vector<pair<int,int> > ranked_rated_1;
+    vector<pair<int,int> > ranked_rated_2;
+    
+    for (int i = 0; i < p.size(); i++)
+        ranked_rated_1.push_back(make_pair(p[i].first,i));
+   
+    for (int i = 0; i < r.size(); i++)
+        ranked_rated_2.push_back(make_pair(r[i].first,i));
+    
+    //ordinando i vettori in ordine di item
+    sort(ranked_rated_1.begin(),ranked_rated_1.end(),comparison_func_item);
+    sort(ranked_rated_2.begin(),ranked_rated_2.end(),comparison_func_item);
+    
+    //l'item minimo e massimo che hanno possibilità di essere stati valutati da entrambi classifiche
+    int it_min = max(ranked_rated_2[0].first,ranked_rated_1[0].first);
+    int it_max = min(ranked_rated_1.back().first,ranked_rated_2.back().first);
+    
+    //Questi vettori conterrano i rank degli item valutati in entrambi i vettori con ogni indice rappresentando un item (ci saranno solo item valutati realmente
+    //e previsti)
+    vector<int> p_to_send;
+    vector<int> r_to_send;
+    
+    int j1=0;
+    int j2=0;
+    
+    
+    /*
+    In questo ciclo l'idea è andare a valutare nei vettori ordinati per item, qualli sono gli elementi che sono valutati (presenti) in entrambi vettori
+    si noti che abbiamo già escluso tutti gli elementi non valutati di ogni vettore ora bisogna solo "filtrare" qualli sono gli item presenti in entrambi
+    Quindi l'idea è fare un ciclo sugli item (i) che possono essere presenti in entrambi vettori (sicuramente compresi tra it_min e it_max)
+    Quindi si procede valutando ogni elemento dei vettori:
+    Es.:
+    v1 = (1,5),(3,3),(4,2),(7,4),(10,6),(12,1)
+    v2 = (2,1),(3,2),(5,3),(7,4)
+    Mio ciclo "for" andrà da 2 a 7 (che sono gli elementi possibilmente present in entrambi vettori)
+    quindi con i contatori j1 e j2 partendo da 0 verifico ongi elemento dentro il ciclo while:
+        1ºloop while: (1 ==) ranked_rated_1[j1].first < 2 &&  (2 ==) ranked_rated_2[j2].first == 2 (entro nel loop) e ricado nel 4º caso
+        2ºloop while: (3 ==) ranked_rated_1[j1].first > 2 &&  (2 ==) ranked_rated_2[j2].first == 2 (non entro nel loop) -> i++ (i == 3)
+        1ºloop while: (3 ==) ranked_rated_1[j1].first == 3 &&  (2 ==) ranked_rated_2[j2].first < 3 (entro nel loop nel 3º caso)
+        2ºloop while: (3 ==) ranked_rated_1[j1].first == 3 &&  (3 ==) ranked_rated_2[j2].first == 3 (entro nel loop nel 1º caso e includo nei miei vettori da spedire
+         il rank del vettore predetto e del vettore reale)
+     
+     Continuando il raggionamento i miei vettori risultanti saranno:
+        p_to_send = 3,4;
+        r_to_send = 2,4;
+     */
+    
+    for (int i = it_min; i <= it_max; i++) {
+        
+        while (ranked_rated_1[j1].first <= i && ranked_rated_2[j2].first <= i) {
+            
+            
+            if (ranked_rated_1[j1].first == i && ranked_rated_2[j2].first == i) {
+                p_to_send.push_back(ranked_rated_1[j1].second);
+                r_to_send.push_back(ranked_rated_2[j2].second);
+                j1++;
+                j2++;
+            }
+            
+            else if (ranked_rated_1[j1].first < i && ranked_rated_2[j2].first < i){
+                j1++;
+                j2++;
+            }
+            
+            else if (ranked_rated_1[j1].first == i && ranked_rated_2[j2].first < i){
+                j2++;
+            }
+            
+            else if (ranked_rated_1[j1].first < i && ranked_rated_2[j2].first == i){
+                j1++;
+            }
+            
+        }
+    }
+    
+    //A questo punto chiamiamo la funzione s_rho:
+    
+    return s_rho (p_to_send,r_to_send);
     
 };
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include<iostream>
 #include<fstream>
+#include<iomanip>
 #include "RMS_testing_2.h"
 
+
 using namespace std;
+
+double fRand(double fMin, double fMax)
+{
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+};
+
+//Per provare genero due vettori
+void rand_vec (vector<pair<int,double> > & v1, vector<pair<int,double> > & v2,int size_vec) {
+    
+    
+    for (int i = 0; i < size_vec; i++) {
+        
+        double rnd = fRand(-1,5);
+        if (rnd < 0) {
+            rnd = -1;
+        }
+        
+        v1.push_back(make_pair(i+1,rnd));
+        
+        rnd = fRand(-1,5);
+        if (rnd < 0) {
+            rnd = -1;
+        }
+        
+        v2.push_back(make_pair(i+1,rnd));
+    }
+
+
+};
+
+
 
 int main() {
     
@@ -53,6 +87,44 @@ int main() {
     cout<<"The ARHR is: "<<RMS_ARHR(v1,10)<<endl;
     
     cout<<"The ARP is: "<<RMS_ARP(v1,10)<<endl;
+    
+    cout<<"The DCG is: "<<RMS_DCG(v1,10)<<endl;
+    
+    vector<pair<int,double> > v3, v4;
+    
+    rand_vec(v3,v4,6);
+    
+    cout<<endl<<endl<<"Vectors: "<<endl;
+    
+    for (int i = 0; i < v3.size(); i++) {
+        cout <<v3[i].first<<"\t";
+    }
+    
+    cout<<endl;
+    
+    for (int i = 0; i < v3.size(); i++) {
+        cout <<setprecision(4)<<v3[i].second<<"\t";
+    }
+    
+    cout<<endl<<endl;
+    
+    for (int i = 0; i < v4.size(); i++) {
+        cout <<v4[i].first<<"\t";
+    }
+    
+    cout<<endl;
+    
+    for (int i = 0; i < v4.size(); i++) {
+        cout <<setprecision(4)<<v4[i].second<<"\t";
+    }
+    
+    cout<<endl<<endl;
+    
+    
+    cout<<"Spearsman rho: "<<compute_s_rho(v3,v4)<<endl;
+    
+    
+    //draw_ROC(v1,v2); ->NOT WORKING
     
     //cout<<"Fallout is: "<<RMS_fallout_robust(v2,10)<<endl;
     
